fb5c5b495454e98ab8a90f79216921f285ef0b39
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2023-02-04 06:57:58 +0900

    production_console.sh も DOCKER_HOST のかわりに SSH_HOST を使用するようにした

--- production_console.sh
+++ production_console.sh
@@ -4,6 +4,15 @@ set -eu
 
 . production.env
 
+: "${SSH_HOST}"
+: "${COMPOSE_PROJECT_NAME}"
+: "${APP_WEB_PORT}"
+: "${SECRET_KEY_BASE}"
+: "${TWITTER_CLIENT_ID}"
+: "${TWITTER_CLIENT_SECRET}"
+
+DOCKER_HOST="ssh://${SSH_HOST}"
+
 export DOCKER_HOST
 export COMPOSE_PROJECT_NAME
 export APP_WEB_PORT
