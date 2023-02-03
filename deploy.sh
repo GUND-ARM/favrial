e8c0857342027908c822fcb1a22b2028fc3e031b
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2023-02-04 06:16:19 +0900

    deployワークフローを追加した

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,12 @@
 
 set -eu
 
-. production.env
-
-export DOCKER_HOST
-export COMPOSE_PROJECT_NAME
-export APP_WEB_PORT
-export SECRET_KEY_BASE
-export TWITTER_CLIENT_ID
-export TWITTER_CLIENT_SECRET
+: "${DOCKER_HOST}"
+: "${COMPOSE_PROJECT_NAME}"
+: "${APP_WEB_PORT}"
+: "${SECRET_KEY_BASE}"
+: "${TWITTER_CLIENT_ID}"
+: "${TWITTER_CLIENT_SECRET}"
 
 docker_compose() {
   docker compose -f docker-compose.yml -f production.yml "$@"
