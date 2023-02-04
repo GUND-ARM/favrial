32b8e8bd85b90cc9876eb6037836f4fa8a6b317f
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2023-02-05 05:15:41 +0900

    タイムライン取得ジョブを複数ユーザに対応させた

--- app/jobs/fetch_timeline_job.rb
+++ app/jobs/fetch_timeline_job.rb
@@ -2,6 +2,8 @@ class FetchTimelineJob < ApplicationJob
   queue_as :default
 
   def perform(count = 10)
-    Tweet.import_timelines(count)
+    User.all.each do |user|
+      user.fetch_reverse_chronological_timelines(count)
+    end
   end
 end
