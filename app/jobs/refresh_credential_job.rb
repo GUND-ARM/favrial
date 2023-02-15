75dca922d84e50511c92dc7e8cb824180d8dd29f
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2023-02-16 06:43:49 +0900

    credentialがnilのときにtokenのリフレッシュを行わないようにした

--- app/jobs/refresh_credential_job.rb
+++ app/jobs/refresh_credential_job.rb
@@ -3,10 +3,15 @@ class RefreshCredentialJob < ApplicationJob
 
   def perform(*args)
     User.all.each do |user|
+      if user.credential.nil?
+        Rails.logger.info "Skipping user id: #{user.id} username: #{user.username} because they have no credential"
+        next
+      end
+
       credential = user.credential
       if credential.expired?
         credential.refresh
-        Rails.logger.info "Refreshed credential for user id: #{user.id} name: #{user.name}"
+        Rails.logger.info "Refreshed credential for user id: #{user.id} username: #{user.username}"
       end
     end
   end
