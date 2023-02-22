116f152d2614712e5d07f3586dffe86abc6f4d27
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2023-02-22 22:06:36 +0900

    バックアップスクリプトを実装した

---
+++ backup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -eu
+
+: "${SSH_HOST}"
+: "${COMPOSE_PROJECT_NAME}"
+
+docker_compose() {
+  DOCKER_HOST="ssh://${SSH_HOST}"
+  export DOCKER_HOST
+  docker compose -p "${COMPOSE_PROJECT_NAME}" -f docker-compose.yml -f production.yml "$@"
+}
+
+date=$(date '+%Y%m%d%H%M%S')
+filename="favrial_production_${date}.dump"
+docker_compose exec db pg_dump -Fc -U postgres -d app_production > "${filename}"
