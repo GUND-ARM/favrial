56b36c4ca0516664eb5d3f0b36f718f151a8c074
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2023-03-03 18:32:06 +0900

    ランダムに10個ツィートを取得するようにした

--- app/jobs/predict_job.rb
+++ app/jobs/predict_job.rb
@@ -2,7 +2,7 @@ class PredictJob < ApplicationJob
   queue_as :default
 
   def perform(count = 10)
-    Tweet.unclassified_with_photo.limit(count).each do |tweet|
+    Tweet.unclassified_with_photo.order('RANDOM()').limit(count).each do |tweet|
       begin
         tweet.predict
       rescue OpenURI::HTTPError => e
