fd88c0f7a85b1bbdfef5c09c66d75b527bca8e64
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2023-03-07 23:09:43 +0900

    sidekiqのダッシュボードを見れるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+require 'sidekiq/web'
+require 'sidekiq-scheduler/web'
+
 Rails.application.routes.draw do
   root "tweets#index"
 
@@ -7,6 +10,11 @@ Rails.application.routes.draw do
   get '/welcome', to: 'welcome#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+
+  mount Sidekiq::Web => '/sidekiq', constraints: lambda { |req|
+    # FIXME: user.admin? で管理者かどうかを判断できるようにする
+    req.session['user_id'].present? and req.session['user_id'] == 3
+  }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
