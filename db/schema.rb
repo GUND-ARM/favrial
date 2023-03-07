ff6933d33b1000d13b844afc2a9fa2bd62d509e3
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2023-03-08 03:55:28 +0900

    users.uid にインデックスを追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_182922) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_185207) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_182922) do
     t.string "url"
     t.string "description"
     t.string "profile_image_url"
+    t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
   add_foreign_key "credentials", "users"
