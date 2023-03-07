d82ea1814e64e10ac80f0f8c71b0415b872dabfb
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2023-03-08 02:56:32 +0900

    tweetsのcreated_atにインデックスを追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_144455) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_174427) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_144455) do
     t.datetime "updated_at", null: false
     t.string "first_media_url"
     t.bigint "user_id"
+    t.index ["created_at"], name: "index_tweets_on_created_at"
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
