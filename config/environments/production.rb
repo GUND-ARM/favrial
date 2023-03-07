64384ca8033565618006d38e123d907434c5aab2
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2023-03-08 01:16:18 +0900

    productionのログレベルを変更した

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,9 @@ Rails.application.configure do
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
-  config.log_level = :info
+  # FIXME: 検証が終わったら :info に戻す
+  #config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
