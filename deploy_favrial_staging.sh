ca9fa7c39de8d45ef4e8ddf7eb632b8a05551b98
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2023-03-18 02:46:20 +0900

    ステージング環境デプロイスクリプトを実装した

---
+++ deploy_favrial_staging.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -eu
+
+. staging.env
+
+export SSH_HOST
+export COMPOSE_PROJECT_NAME
+export APP_WEB_PORT
+export SECRET_KEY_BASE
+export TWITTER_CLIENT_ID
+export TWITTER_CLIENT_SECRET
+
+sh -x deploy.sh "$@"
