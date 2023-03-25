8b2fba8c0cb813352d32d138dec074db4e720794
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2023-03-26 03:06:54 +0900

    取得系のジョブでエラーハンドリングをおこなうようにした

--- app/jobs/refresh_credential_job.rb
+++ app/jobs/refresh_credential_job.rb
@@ -3,11 +3,22 @@ class RefreshCredentialJob < ApplicationJob
 
   def perform(*args)
     User.joins(:credential).each do |user|
-      credential = user.credential
-      if credential.expired?
-        credential.refresh
-        Rails.logger.info "Refreshed credential for user id: #{user.id} username: #{user.username}"
+      begin
+        refresh_user_credential(user)
+      rescue => e
+        Rails.logger.error("Error refreshing credential for @#{user.username}: #{e.message}")
+        Rails.logger.error(e.backtrace.join("\n"))
       end
+    end
+  end
+
+  def refresh_user_credential(user)
+    credential = user.credential
+    if credential.expired?
+      credential.refresh
+      Rails.logger.info "Refreshed credential for user id: #{user.id} username: #{user.username}"
+    else
+      Rails.logger.info "Credential for user id: #{user.id} username: #{user.username} is not expired"
     end
   end
 end
