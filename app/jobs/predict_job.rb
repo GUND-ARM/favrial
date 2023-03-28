9ce11b5c2dfc60e42ef5c85eb8b07957e20205ae
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2023-03-29 02:33:30 +0900

    Predictjob 1回あたりの画像分類実行数を30回に増やした

--- app/jobs/predict_job.rb
+++ app/jobs/predict_job.rb
@@ -1,12 +1,11 @@
 class PredictJob < ApplicationJob
   queue_as :default
 
-  def perform(count = 10)
+  def perform(count = 30)
     Tweet.unclassified_with_photo.order('RANDOM()').limit(count).each do |tweet|
       begin
         tweet.predict
       rescue OpenURI::HTTPError => e
-        Rails.logger.error e
         Rails.logger.error("Error predicting for id: #{tweet.id}: #{e.message}")
         #Rails.logger.error(e.backtrace.join("\n"))
       end
