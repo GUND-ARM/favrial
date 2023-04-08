4d47424ed9de24b3adcda77cf99dcdd2d6f76d89
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2023-04-08 18:17:43 +0900

    ベータユーザ以外ログインできないことをチェックするテストをはずした

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -5,24 +5,30 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     Rails.configuration.public_release = false
   end
 
-  test "should create session if beta user" do
-    use_omniauth(auth_hash)
-    get "/auth/twitter2/callback"
-    assert_response :redirect
-  end
+  #test "should create session if beta user" do
+  #  use_omniauth(auth_hash)
+  #  get "/auth/twitter2/callback"
+  #  assert_response :redirect
+  #end
 
-  test "should not create session if not beta user" do
+  #test "should not create session if not beta user" do
+  #  use_omniauth(auth_hash_not_beta_user)
+  #  get "/auth/twitter2/callback"
+  #  assert_response :unauthorized
+  #end
+
+  #test "should create session if public release with non beta user" do
+  #  Rails.configuration.public_release = true
+  #  use_omniauth(auth_hash_not_beta_user)
+  #  get "/auth/twitter2/callback"
+  #  assert_response :redirect
+  #  Rails.configuration.public_release = false
+  #end
+
+  test "should create session with non beta user" do
     use_omniauth(auth_hash_not_beta_user)
     get "/auth/twitter2/callback"
-    assert_response :unauthorized
-  end
-
-  test "should create session if public release with non beta user" do
-    Rails.configuration.public_release = true
-    use_omniauth(auth_hash_not_beta_user)
-    get "/auth/twitter2/callback"
     assert_response :redirect
-    Rails.configuration.public_release = false
   end
 
   test "should destroy session" do
